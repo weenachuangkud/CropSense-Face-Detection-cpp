6f826b57a558702231ba7e6cb07ff5de51bdc422
Mawin CK <weena.chuangkud@gmail.com>
2026-03-24 00:51:45 +0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,7 @@
 #include <opencv2/opencv.hpp>
+#include <nlohmann/json.hpp>
 
 int main()
 {
-    cv::VideoCapture cap(0); // 0 = default webcam
-    cv::Mat frame;
-
-    while(true)
-    {
-        cap >> frame;
-        cv::imshow("Webcam", frame);
-
-        if(cv::waitKey(1) == 27) // press ESC to quit
-            break;
-    }
-
     return 0;
 }
