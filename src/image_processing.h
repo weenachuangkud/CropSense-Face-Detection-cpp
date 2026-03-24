eba89583fb966272b20faa27a23778e7ae556400
Mawin CK <weena.chuangkud@gmail.com>
2026-03-25 04:41:27 +0700

    Implementation

--- src/image_processing.h
+++ src/image_processing.h
@@ -1,6 +1,6 @@
 #pragma once
-
-#include<string>
+#include <opencv2/opencv.hpp>
+#include <string>
 #include "crop_config.h"
 #include "user_input.h"
 
@@ -14,13 +14,39 @@ struct ImageProcessConfig {
     int min_fullbody_res;
 };
 
-void images_error(
+void link_image_error(
     const std::string& image_path,
     const std::string& error_folder
 );
+
 int process_image(
     const std::string& image_path,
     const CropConfig& crop,
     const ImageProcessConfig& process_config,
     const PreviewConfig& preview_config
+);
+
+bool draw_rectangle(
+    const int& endX,
+    const int& startX,
+    const int& endY,
+    const int& startY,
+    cv::Mat& image,
+    const std::string& image_path,
+    const std::string& filename,
+    const CropConfig& crop,
+    const ImageProcessConfig& process_config,
+    const PreviewConfig& preview_config,
+    bool is_error,
+    int face_index,
+    float confidence,
+    const std::string& error_msg
+);
+
+void preview(
+    const cv::Mat& debug_image,
+    const cv::Mat& resized_image,
+    const int& preview_output_res,
+    const int& preview_debug_max_res,
+    bool is_error
 );
